f529c7994f97cdaec5c60f768ca138a92266f208
juicy <wopjr1234@gmail.com>
2025-02-14 14:44:42 -0500

    UI is complete and finished

--- helios.py
+++ helios.py
@@ -1,8 +1,31 @@
 
+# imports
 import os
-f = open("pic.pic", "r")
-test = f.read()
+import platform
 
-os.system('clear')
-os.system('cls')
-print(test)
+# Grabbing prompt picture
+f = open("pic.pic", "r")
+draw = f.read()
+
+# platform check for clearing the console
+if platform.system() == "Linux":
+    command_clear = "clear"
+else:
+    command_clear = "cls"
+
+# clear the terminal
+os.system(command_clear)
+
+# draw the terminal
+print(draw)
+
+# main loop for program
+while True:
+    command = input("Command: ")
+    if command == "quit":
+        break
+    elif command == "clear":
+        os.system(command_clear)
+        print(draw)
+    else:
+        print(command)
