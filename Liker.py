7871601b73743d285722b184f3be745c397521af
Ali Donald Curtis <89867025+AliCurtis5747@users.noreply.github.com>
2025-03-19 11:14:16 -0400

    Update Liker.py
    Implementation is new
    Is passed a bot from the bot array, and uses the credentials to login. 
    Is passed a link and uses that to like a specific post

--- Liker.py
+++ Liker.py
@@ -1,16 +1,17 @@
 import instagrapi
 from instagrapi import Client
 
+import instagrapi
+from instagrapi import Client
+
 def liker(bots, link):
-    try:
-        for x in range(0, len(bots)):
-            bots[x].media_like(link)
-        print("The operation has been completed")
-    except:
-        print("The operation has failed")
-        return 1
-    return 0
+    botInstant = Client(request_timeout = 60)
+    botInstant.login(bots.email, bots.password)
+    LikeID = botInstant.media_id(botInstant.media_pk_from_url(link))
+    botInstant.media_like(LikeID)
+    print("This operation was completed successfully!")
 
         
 
-#This is the main "Like" function. It will take one of the bots/bot arrays and have them like a post declared by a hyperlink
+#This is the main "Like" function. It will take one of the bot objects, use that to login, and then like a post declared by a hyperlink
+        
