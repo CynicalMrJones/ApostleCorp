32dcb82f27e87a72ed4840364d8f4ed0aadc9144
Ali Donald Curtis <89867025+AliCurtis5747@users.noreply.github.com>
2025-03-26 00:38:18 -0400

    Add files via upload
    This is the poster function to be tested and iterated on

---
+++ Poster.py
@@ -0,0 +1,10 @@
+import instagrapi
+from instagrapi import Client
+
+def poster(bots, link):
+    botInstant = Client(request_timeout = 60)
+    botInstant.login(bots.email, bots.password)
+    img = input("Please input the path to your photo (must be a .jpg or a .jpeg)")
+    capt = input("Input the photo caption")
+    botInstant.photo_upload(img, capt)
+    print("This operation was completed successfully!")
