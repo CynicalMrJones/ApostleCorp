e084b657a86b4e776cf02d4073ceda2a49ee34a7
Ali Donald Curtis <89867025+AliCurtis5747@users.noreply.github.com>
2025-03-26 00:39:04 -0400

    Update Poster.py
    Forgot to edit arguments

--- Poster.py
+++ Poster.py
@@ -1,7 +1,7 @@
 import instagrapi
 from instagrapi import Client
 
-def poster(bots, link):
+def poster(bots):
     botInstant = Client(request_timeout = 60)
     botInstant.login(bots.email, bots.password)
     img = input("Please input the path to your photo (must be a .jpg or a .jpeg)")
