b47ca5e77b2acfb611457532af1ea63a81dfe625
Ali Donald Curtis <89867025+AliCurtis5747@users.noreply.github.com>
2025-04-05 09:11:41 -0400

    Update Commenter.py
    massComment function is defined and functional

--- Commenter.py
+++ Commenter.py
@@ -6,9 +6,26 @@ def commenter(bots, link, comment):
     botInstant = Client(request_timeout=60)
     try:
         botInstant.login(bots.email, bots.password)
+        alink = botInstant.media_pk_from_url(link)
+        mediaID = botInstant.media_id(alink)
+        botInstant.media_comment(mediaID, comment)
     except:
-        print(f'{bots.email} failed to post a comment')
+        print(f'{bots.email} failed to post a comment ')
 
-    postedComment = botInstant.media_comment(CommentID, comment)
-    #postedComment.dict()
     print("The operation was completed successfully")
+def massComment(bots, link, comments):
+    commentRay = {}
+    count = 0
+    with open(comments, "r") as file:
+        
+        for line in file:
+            commentRay[count] = line
+            count += 1
+    count = 0
+    for i in bots:
+        try:
+            commenter(bots[i], link, commentRay[count])
+            count += 1
+        except:
+            print("There was an error. This operation is terminated")
+    
