1c79fe35240f37cb5a98acfc27aec0ef78fcd5d1
AliCurtis5747 <githubAli882@gmail.com>
2025-04-16 11:03:12 -0400

    Hello

--- Poster.py
+++ Poster.py
@@ -5,10 +5,11 @@ def poster(bots):
     botInstant = Client(request_timeout=60)
     try:
         botInstant.login(bots.email, bots.password)
+        img = input("Please input the path to your photo (must be a .jpg or a .jpeg)")
+        capt = input("Input the photo caption :: ")
+        botInstant.photo_upload(img, capt)
+        print("This operation was completed successfully!")
     except:
         print(f'{bots.email} failed to login')
 
-    img = input("Please input the path to your photo (must be a .jpg or a .jpeg)")
-    capt = input("Input the photo caption")
-    botInstant.photo_upload(img, capt)
-    print("This operation was completed successfully!")
+
