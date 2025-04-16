4678b334bf8d863cce49dd3feb031fcf7079489f
CynicalMrJones <wopjr1234@gmail.com>
2025-04-16 15:54:49 -0400

    Moved files into folders for easier viewing

---
+++ functions/Liker.py
@@ -0,0 +1,17 @@
+from instagrapi import Client
+# This is the main "Like" function. It will take one of the bot objects, use that to login, and then like a post declared by a hyperlink
+
+
+def liker(bots, link):
+    try:
+        botInstant = Client(request_timeout=60)
+        try:
+            botInstant.login(bots.email, bots.password)
+        except:
+            print(f'{bots.email} failed to login')
+            return -1
+        LikeID = botInstant.media_id(botInstant.media_pk_from_url(link))
+        botInstant.media_like(LikeID)
+        print(f'{bots.email} liked the media')
+    except:
+        print(f'{bots.email} failed to like the media')
