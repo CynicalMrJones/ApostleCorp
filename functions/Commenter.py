79e4a647096cf8de40af079d859d70db2b573388
juicy <wopjr1234@gmail.com>
2025-04-18 16:19:38 -0400

    Fixed comment problem

--- functions/Commenter.py
+++ functions/Commenter.py
@@ -13,7 +13,7 @@ def commenter(bots, link, comment):
         botInstant.login(bots.email, bots.password)
         alink = botInstant.media_pk_from_url(link)
         mediaID = botInstant.media_id(alink)
-        botInstant.media_comment(mediaID, dirname + "/commentPrompts/" + comment)
+        botInstant.media_comment(mediaID, comment)
         print("The operation was completed successfully")
     except:
         print(f'{bots.email} failed to post a comment ')
@@ -23,11 +23,14 @@ def massComment(bots, link, comments):
     dirname = os.getcwd()
     commentRay = {}
     count = 0
-    with open(dirname + "/commentPrompts/" + comments, "r") as file:
-        for line in file:
-            commentRay[count] = line
-            count += 1
-    count = 0
+    try:
+        with open(dirname + "/commentPrompts/" + comments, "r") as file:
+            for line in file:
+                commentRay[count] = line
+                count += 1
+        count = 0
+    except:
+        print("File not found, make sure that it is in commentPrompts folder")
     try:
         for i in bots:
             count = random.randrange(0, len(commentRay))
